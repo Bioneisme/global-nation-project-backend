7b796a6dde621aca5f14a575425eee49d46bb876
Bioneisme <bioneisme@gmail.com>
2022-06-30 13:34:12 +0600

    session storage migrated from Mongo to Redis

--- server/server.js
+++ server/server.js
@@ -3,46 +3,54 @@ const mongoose = require('mongoose')
 const passport = require('passport')
 const session = require('express-session')
 const cookieParser = require('cookie-parser')
-const MongoStore = require('connect-mongo')
+const RedisStore = require('connect-redis')(session);
 const bodyParser = require('body-parser')
 const cors = require('cors')
+
 const authRoute = require('./routes/auth-route')
-const pageRoute = require('./routes/page-route')
+const userRoute = require('./routes/user-route')
 require('dotenv').config()
 require('./controllers/passport-controller')(passport)
 
 const app = express()
 const PORT = process.env.PORT
 
-const corsOptions = {
+const redisOptions = {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PASSWORD,
+    logErrors: true,
+}
+
+app.use(cors({
     credentials: true,
     origin: process.env.CLIENT_URL,
-}
-app.use(cors(corsOptions))
+}))
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use(cookieParser())
 app.use(session({
     secret: process.env.SESSION_SECRET,
-    store: MongoStore.create({
-        mongoUrl: process.env.DB_URL,
-        ttl: 7 * 24 *  60 * 60, // 7 Days
-    }),
+    store: new RedisStore(redisOptions),
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 24 * 60 * 60 * 1000 // 1d
+    }
 }));
 
 app.use(passport.initialize())
 app.use(passport.session())
 
-const start = async () => {
+async function start() {
     try {
         await mongoose.connect(process.env.DB_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
-        app.listen(PORT, () => console.log("http://localhost:" + PORT))
+        app.listen(PORT, () => console.log(process.env.SERVER_URL))
     } catch (e) {
         console.log(e)
         process.exit(1)
@@ -50,6 +58,6 @@ const start = async () => {
 }
 
 app.use('/api', authRoute)
-app.use('/api', pageRoute)
+app.use('/api', userRoute)
 
 start()
