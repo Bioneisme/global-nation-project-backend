081054f2e849db42262a62c30ff2bbd693fd99d7
Bioneisme <bioneisme@gmail.com>
2022-07-10 14:41:47 +0600

    Getting user courses

--- server/routes/user-route.js
+++ server/routes/user-route.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const router = express.Router()
+
 const userController = require('../controllers/user-controller')
 
 router.get('/getUser', userController.getUser)
+
+router.get('/getUserCourses', userController.getUserCourses)
 
 module.exports = router
