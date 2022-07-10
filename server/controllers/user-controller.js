94c6c0ddbe8e8f0992a1036b0ebb99071f287c55
Bioneisme <bioneisme@gmail.com>
2022-07-10 14:41:42 +0600

    Getting user courses

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,9 +1,24 @@
+const Course = require('../models/course-model')
+
 class UserController {
     async getUser(req, res, next) {
         try {
             if (!req.user)
                 return res.status(404).json({'message': 'User not found'})
             return res.status(200).json(req.user)
+        }
+        catch (e) {
+            next(e)
+        }
+    }
+
+    async getUserCourses(req, res, next) {
+        try {
+            if (!req.user)
+                return res.status(404).json({'message': 'User not found'})
+
+            const courses = await Course.find({author: req.user})
+            return res.status(200).json(courses)
         }
         catch (e) {
             next(e)
