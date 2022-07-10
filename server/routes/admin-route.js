48a724e1a5075253a33a093a5d312ebab03bbac6
Bioneisme <bioneisme@gmail.com>
2022-07-10 14:42:15 +0600

    Getting user by email

--- server/routes/admin-route.js
+++ server/routes/admin-route.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+
 const adminController = require('../controllers/admin-controller')
 
 router.get('/getUsers', adminController.getUsers)
