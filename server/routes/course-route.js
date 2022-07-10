c5a9fe71515fca2126723a15c25d4cc455381f71
Bioneisme <bioneisme@gmail.com>
2022-07-10 14:44:02 +0600

    Getting course to edit by id

--- server/routes/course-route.js
+++ server/routes/course-route.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
+const {isAuth} = require('../middlewares/permissions-middleware')
+const courseController = require('../controllers/course-controller')
 
+router.post('/createCourse', isAuth, courseController.createCourse)
+
+router.get('/getCourseToEdit/:id', courseController.getCourseToEdit)
 
 module.exports = router
