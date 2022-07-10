d423f3715d7c7e3432b1ad8a29e186935bd8b3db
Bioneisme <bioneisme@gmail.com>
2022-07-10 14:40:38 +0600

    Authorization checker

--- server/middlewares/permissions-middleware.js
+++ server/middlewares/permissions-middleware.js
@@ -1,6 +1,9 @@
 class PermissionsMiddleware {
     async isAdmin(req, res, next) {
         try {
+            if (!req.user)
+                return res.status(400).json({'message': 'You must be logged in'})
+
             const userRoles = req.user.roles;
 
             let isAdmin = false
@@ -9,9 +12,18 @@ class PermissionsMiddleware {
                     isAdmin = true
                 }
             })
-
             if (isAdmin) return next()
             return res.status(400).json({'message': 'You don\'t have access!'})
+        } catch (e) {
+            next(e)
+        }
+    }
+
+    async isAuth(req, res, next) {
+        try {
+            if (!req.user)
+                return res.status(400).json({'message': 'You must be logged in'})
+            return next()
         } catch (e) {
             next(e)
         }
